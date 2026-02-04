0c73ce88ef04b62c7ba5f9ad8bc98980df4bf81c
Boghan Polikarpov <Bogdanpolikarpow@gmail.com>
2026-02-04 14:52:18 +0100

    Created tests (testGetCatalogReturnsOkAndJson, testGetCatalogReturnsEmptyList) for AdvertisementController

--- src/test/java/com/khpi/wanderua/controller/AdvertisementControllerTest.java
+++ src/test/java/com/khpi/wanderua/controller/AdvertisementControllerTest.java
@@ -7,6 +7,7 @@ import com.khpi.wanderua.enums.AdvertisementType;
 import com.khpi.wanderua.service.AdvertisementService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AdvertisementController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class AdvertisementControllerTest {
 
     @Autowired
