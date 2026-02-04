60f790ed26a71db64f55e084e9af5fdda421fc7d
Boghan Polikarpov <Bogdanpolikarpow@gmail.com>
2026-02-04 15:36:57 +0100

    Updated tests for AdvertisementController

--- src/test/java/com/khpi/wanderua/controller/AdvertisementControllerTest.java
+++ src/test/java/com/khpi/wanderua/controller/AdvertisementControllerTest.java
@@ -71,7 +71,7 @@ class AdvertisementControllerTest {
                 .thenReturn(sampleResponse);
 
         // Act & Assert: Perform GET request and expect 200 OK and JSON content
-        mockMvc.perform(get("/advertisements/catalog")
+        mockMvc.perform(get("/api/advertisements/catalog")
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print()) // Print request and response for debugging
                 .andExpect(status().isOk())
